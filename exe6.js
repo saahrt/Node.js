f8ca596d6e7ca1f04baabe60271170beb274e012
otaviojnjr <otaviojnjunior@gmail.com>
2024-09-17 20:29:53 -0300

    Node.js

--- exe6.js
+++ exe6.js
@@ -1,8 +1,23 @@
 //"Calcular o Fatorial de um Número"
 function fatorial(n){
     if(n < 0){//verifica se o número é negativo
-        console.log(`Error: ${n} é um número negativo`);
+        throw new Error(`O fatorial não está definido para números negativos.`);
+    }else if (n === 0 || n === 1) {//verirficar se o número é igual a 1 ou 0
+        return 1;
+    }else{
+        let resultado = 1; // Inicializa o resultado como 1
+        //calcular o fatorial
+        for (let i = 2; i <= n; i++) {
+            resultado *= i; // Multiplica o resultado pelo valor atual de i
+        }
+        
+        return resultado;
     }
-    var resultado = 1;
-
+}
+const num = 5
+try {
+    const resultado = fatorial(num);
+    console.log(`O fatorial do número ${num} é igual ${resultado}`);
+} catch (error) {
+    console.error(error.message); // Imprime a mensagem de erro no console
 }
---
+++ exe8.js
@@ -0,0 +1,5 @@
+//"Converter Temperatura de Celsius para Fahrenheit"
+function CelToFar(celsius){
+    return celsius * (9 / 5) + 32;
+}
+console.log(CelToFar(25));
